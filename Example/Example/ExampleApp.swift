c8b95fd4f09575da4786835cde6cc6f169e675d4
Alrik Firl <alrik.firl@hover.to>
2024-10-31 23:01:50 -0700

    add background uploading capability and streamline some of the Example app code

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -12,7 +12,11 @@ import BackgroundTasks
 @main
 struct ExampleApp: App {
     
+    let uploadTaskIdentifier = "to.hover.uploads"
+    
     init() {
+        // NOTE: to allow the SDK to handle its own background uploads, uncomment this line
+        // and comment out the `registerBackgroundTask` line.
         //HVPartnerSDK.sharedInstance.registerForBackgroundJobs()
         HVPartnerSDK.sharedInstance.initialize()
         
@@ -27,19 +31,15 @@ struct ExampleApp: App {
 }
 
 extension ExampleApp {
+    /// Example of how we can manually trigger the SDK to perform background uploads
     func registerBackgroundTask() {
-        // NOTE: can configure whether to upload on wifi only (`true`) vs. wifi + cellular (`false`)
-        let uploadOnlyOnWiFi = true
-        HVPartnerSDK.sharedInstance.initialize(parameters: .init(
-            uploadOnWiFiOnly: uploadOnlyOnWiFi))
-        
         // set up background tasks for scheduling HVCaptureSDK uploads, if applicable
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "to.hover.uploads", using: nil) { task in
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: uploadTaskIdentifier, using: nil) { task in
             self.uploadCaptureDataInBackground(task: task as! BGProcessingTask)
         }
         
         // schedule the background task to be executed
-        let request = BGProcessingTaskRequest(identifier: "to.hover.uploads")
+        let request = BGProcessingTaskRequest(identifier: uploadTaskIdentifier)
         request.requiresNetworkConnectivity = true
         request.requiresExternalPower = false
         try? BGTaskScheduler.shared.submit(request)
@@ -49,6 +49,7 @@ extension ExampleApp {
         let processingTask = Task {
             do {
                 // set up HVPartnerSDK for background uploading (on wifi only)
+                // NOTE: can configure whether to upload on wifi only (`true`) vs. wifi + cellular (`false`)
                 try await HVPartnerSDK.sharedInstance.initializeForBackground(parameters: .init(
                     uploadOnWiFiOnly: true))
                 task.setTaskCompleted(success: true)
